faab461b97569d9b6369967a0da9be0386770142
Philip Bizimis <bizimisphilip@gmail.com>
2019-04-11 13:35:05 +0200

    navbar update

--- irtp/static/index.js
+++ irtp/static/index.js
@@ -11,8 +11,10 @@ function toggleNavbar() {
     var element = document.getElementById("test");
     if(element.classList.contains("is-active")) {
         element.classList.remove("is-active");
+        document.getElementById("sidenav").style.width = "0";
     } else {
         element.classList.add("is-active");
+        document.getElementById("sidenav").style.width = "100%";
     }
-    
 }
+    
