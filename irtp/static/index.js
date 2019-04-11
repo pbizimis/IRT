6892413a80eadcfefb16b2ee43c62b1860347ba4
Philip Bizimis <bizimisphilip@gmail.com>
2019-04-11 15:21:22 +0200

    navbar technic fixed

--- irtp/static/index.js
+++ irtp/static/index.js
@@ -1,9 +1,11 @@
 function openNav() {
     document.getElementById("sidenav").style.right = "0";
+    document.getElementById("body").style.overflow = "hidden";
 }
 
 function closeNav() {
     document.getElementById("sidenav").style.right = "100%";
+    document.getElementById("body").style.overflow = "visible";
 }
 
 
