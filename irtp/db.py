d061428d253d3d17d2a6162d8b1b92892a5f6dc9
pbizimis <bizimisphilip@gmail.com>
2019-04-13 18:12:41 +0200

    press

--- irtp/db.py
+++ irtp/db.py
@@ -9,8 +9,12 @@ def get_db():
     return db, db_cursor
 
 def get_articles():
+
     db, db_cursor = get_db()
     db_cursor.execute(GET_ARTICLES)
     articles = db_cursor.fetchall()
+    for i, article in enumerate(articles):
+        articles[i] = list(article)
+        articles[i][2] = article[2].split(".")
     db.close()
     return articles
