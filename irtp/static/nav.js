26bd7fcc583f6c2e0ade338a2d50a0ca4b467d55
pbizimis <bizimisphilip@gmail.com>
2019-04-17 19:48:06 +0200

    css linking done

---
+++ irtp/static/nav.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sidenav").style.right = "0";
+    document.getElementById("body").style.overflow = "hidden";
+}
+
+function closeNav() {
+    document.getElementById("sidenav").style.right = "100%";
+    document.getElementById("body").style.overflow = "visible";
+}
