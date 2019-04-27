63e1f9f371ce1a3bce7c3b25b7e225e4d208eef2
Philip Bizimis <bizimisphilip@gmail.com>
2019-04-27 19:00:15 +0200

    navbar update

--- irtp/static/nav.js
+++ irtp/static/nav.js
@@ -7,3 +7,15 @@ function closeNav() {
     document.getElementById("sidenav").style.right = "100%";
     document.getElementById("body").style.overflow = "visible";
 }
+
+function togglePanel() {
+    var panel = document.getElementsByClassName("panel-konzept")[0];
+    var plus = document.getElementsByClassName("plus")[0];
+    if (panel.style.maxHeight){
+        panel.style.maxHeight = null;
+        plus.style.transform = "rotate(0deg)";
+    } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+        plus.style.transform = "rotate(45deg)";
+    }
+}
